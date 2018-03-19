4a8a3761265a211c8aa6efa4ab9f913a7ce06c27
SiewYan <siew.yan.hoh@cern.ch>
2018-03-19 10:15:35 -0500

    adhering to the boosted naming

--- BUILD.sh
+++ BUILD.sh
@@ -2,18 +2,16 @@
 
 set -e 
 
-#for i in boosted_mass0 boosted_mass1 boosted_mass2 boosted_mass3
-#for i in boosted_mass0_subjet boosted_mass1_subjet boosted_mass2_subjet boosted_mass3_subjet
-#for i in boosted_mass0_subjet_2017 boosted_mass1_subjet_2017 boosted_mass2_subjet_2017 boosted_mass3_subjet_2017
+for i in boosted_mass0 boosted_mass1 boosted_mass2 boosted_mass3
+do
+    python buildModel.py $i
+done
 
-#for i in boosted_mass0f boosted_mass0p boosted_mass1f boosted_mass1p boosted_mass2f boosted_mass2p boosted_mass3f boosted_mass3p
-#do
-#    python buildModel.py $i
-#done
+if [-e mono-x.root];then
+    rm mono-x.root
+fi
 
-#rm mono-x.root
-
-hadd mono-x.root mono-x0p.root mono-x1p.root mono-x2p.root mono-x3p.root mono-x0f.root mono-x1f.root mono-x2f.root mono-x3f.root
+hadd mono-x.root boosted-mass0.root boosted-mass1.root boosted-mass2.root boosted-mass3.root
 
 if [ -e mono-x.root ];then
     python runModel_boosted.py
--- datacards/boosted_datacards/massbin_combine.py
+++ datacards/boosted_datacards/massbin_combine.py
@@ -9,25 +9,25 @@ args = parser.parse_args()
 
 naming={
     #config files                            #CR/SR
-    "monox_dielectron_control"             : ["zee"],
-    "monox_dielectron_fail_control"        : ["zee_fail"],
-    "monox_dimuon_control"                 : ["zmm"],
-    "monox_dimuon_fail_control"            : ["zmm_fail"],
-    "monox_signal"                         : ["sig"],
-    "monox_signal_fail"                    : ["sig_fail"],
-    "monox_singleelectrontop_control"      : ["ten"],
-    "monox_singleelectrontop_fail_control" : ["ten_fail"],
-    "monox_singleelectronw_control"        : ["wen"],
-    "monox_singleelectronw_fail_control"   : ["wen_fail"],
-    "monox_singlemuontop_control"          : ["tmn"],
-    "monox_singlemuontop_fail_control"     : ["tmn_fail"],
-    "monox_singlemuonw_control"            : ["wmn"],
-    "monox_singlemuonw_fail_control"       : ["wmn_fail"],
+    "boosted_dielectron_control"             : ["zee"],
+    "boosted_dielectron_fail_control"        : ["zee_fail"],
+    "boosted_dimuon_control"                 : ["zmm"],
+    "boosted_dimuon_fail_control"            : ["zmm_fail"],
+    "boosted_signal"                         : ["sig"],
+    "boosted_signal_fail"                    : ["sig_fail"],
+    "boosted_singleelectrontop_control"      : ["ten"],
+    "boosted_singleelectrontop_fail_control" : ["ten_fail"],
+    "boosted_singleelectronw_control"        : ["wen"],
+    "boosted_singleelectronw_fail_control"   : ["wen_fail"],
+    "boosted_singlemuontop_control"          : ["tmn"],
+    "boosted_singlemuontop_fail_control"     : ["tmn_fail"],
+    "boosted_singlemuonw_control"            : ["wmn"],
+    "boosted_singlemuonw_fail_control"       : ["wmn_fail"],
 }
 command=["combineCards.py"]
 for datacard in os.listdir("./"):
     if datacard.endswith(".txt"):
-        if datacard == "monox_datacard.txt" or datacard == "combined_mass0.txt" or not datacard.startswith('monox'):
+        if datacard == "boosted_datacard.txt" or datacard == "combined_mass0.txt" or not datacard.startswith('boosted'):
             continue
         #print datacard.split('.')[0]
         command.append(args.massbin+"_"+naming[datacard.split('.')[0]][0]+"=./"+datacard)
