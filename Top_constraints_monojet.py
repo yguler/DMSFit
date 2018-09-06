df3f01a6117f876d0eaa425e7967d47b19b406b8
areinsvo <ahall@fnal.gov>
2018-09-05 23:18:27 -0500

    Add 2 tag to 1 tag SF

--- Top_constraints_monojet.py
+++ Top_constraints_monojet.py
@@ -20,33 +20,52 @@ def cmodel(cid,nam,_f,_fOut, out_ws, diag):
   processName = "TTbar" # Give a name of the process being modelled
   metname = "met"    # Observable variable name 
 
-  targetmc     = _fin.Get("signal_ttbar")      # define monimal (MC) of which process this config will model
-  controlmc    = _fin.Get("muonelectron_ttbar")
-  controlmc_e  = _fin.Get("electronmuon_ttbar")
-  controlmc_wm = _fin.Get("singlemuonw_ttbar")
-  controlmc_we = _fin.Get("singleelectronw_ttbar")
- 
+  #define nominal (MC) of which process this config will model
+  targetmc0tag     = _fin.Get("signal0tag_ttbar")
+  targetmc1tag     = _fin.Get("signal1tag_ttbar")
+  targetmc2tag     = _fin.Get("signal2tag_ttbar")
+  #defines in / out acceptance
+  controlmc0tag = _fin.Get("singlemuon0tag_ttbar")
+  controlmc1tag = _fin.Get("singlemuon1tag_ttbar")
+  controlmc2tag = _fin.Get("singlemuon2tag_ttbar")
+
+  controlmc0tag_e = _fin.Get("singleelectron0tag_ttbar")
+  controlmc1tag_e = _fin.Get("singleelectron1tag_ttbar")
+  controlmc2tag_e = _fin.Get("singleelectron2tag_ttbar")
 
 
   # Create the transfer factors and save them (not here you can also create systematic variations of these 
   # transfer factors (named with extention _sysname_Up/Down
 
-  TopScales = targetmc.Clone(); TopScales.SetName("topme_weights_%s"%cid)
-  TopScales.Divide(controlmc); _fOut.WriteTObject(TopScales);
+  TopScales0tag = targetmc0tag.Clone(); TopScales0tag.SetName("topwm0tag_weights_%s"%cid)
+  TopScales0tag.Divide(controlmc0tag); _fOut.WriteTObject(TopScales0tag);
 
-  TopScales_e = targetmc.Clone(); TopScales_e.SetName("topem_weights_%s"%cid)
-  TopScales_e.Divide(controlmc_e); _fOut.WriteTObject(TopScales_e);
+  TopScales0tag_e = targetmc0tag.Clone(); TopScales0tag_e.SetName("topwe0tag_weights_%s"%cid)
+  TopScales0tag_e.Divide(controlmc0tag_e); _fOut.WriteTObject(TopScales0tag_e);
 
-  TopScales_wm = targetmc.Clone(); TopScales_wm.SetName("wm_weights_%s"%cid)
-  TopScales_wm.Divide(controlmc_wm); _fOut.WriteTObject(TopScales_wm);
+  TopScales1tag = targetmc1tag.Clone(); TopScales1tag.SetName("topwm1tag_weights_%s"%cid)
+  TopScales1tag.Divide(controlmc1tag); _fOut.WriteTObject(TopScales1tag);
 
-  TopScales_we = targetmc.Clone(); TopScales_we.SetName("we_weights_%s"%cid)
-  TopScales_we.Divide(controlmc_we); _fOut.WriteTObject(TopScales_we);
+  TopScales1tag_e = targetmc1tag.Clone(); TopScales1tag_e.SetName("topwe1tag_weights_%s"%cid)
+  TopScales1tag_e.Divide(controlmc1tag_e); _fOut.WriteTObject(TopScales1tag_e);
+
+  TopScales2tag = targetmc2tag.Clone(); TopScales2tag.SetName("topwm2tag_weights_%s"%cid)
+  TopScales2tag.Divide(controlmc2tag); _fOut.WriteTObject(TopScales2tag);
+
+  TopScales2tag_e = targetmc2tag.Clone(); TopScales2tag_e.SetName("topwe2tag_weights_%s"%cid)
+  TopScales2tag_e.Divide(controlmc2tag_e); _fOut.WriteTObject(TopScales2tag_e);
+
+  TopScales2tagTo1tag = targetmc1tag.Clone(); TopScales2tagTo1tag.SetName("topwm2tagTo1tag_weights_%s"%cid)
+  TopScales2tagTo1tag.Divide(controlmc2tag); _fOut.WriteTObject(TopScales2tagTo1tag);
+
+  TopScales2tagTo1tag_e = targetmc1tag.Clone(); TopScales2tagTo1tag_e.SetName("topwe2tagTo1tag_weights_%s"%cid)
+  TopScales2tagTo1tag_e.Divide(controlmc2tag_e); _fOut.WriteTObject(TopScales2tagTo1tag_e);
+
   #######################################################################################################
 
   _bins = []  # take bins from some histogram, can choose anything but this is easy 
-  for b in range(targetmc.GetNbinsX()+1):
-    _bins.append(targetmc.GetBinLowEdge(b+1))
+  for b in range(targetmc0tag.GetNbinsX()+1):
+    _bins.append(targetmc0tag.GetBinLowEdge(b+1))
 
   # Here is the important bit which "Builds" the control region, make a list of control regions which 
   # are constraining this process, each "Channel" is created with ...
@@ -55,10 +74,14 @@ def cmodel(cid,nam,_f,_fOut, out_ws, diag):
   # TRANSFERFACTORS are what is created above, eg TopScales
 
   CRs = [
-   Channel("muonelectronModel",      _wspace,out_ws,cid+'_'+model,TopScales),
-   Channel("electronmuonModel",  _wspace,out_ws,cid+'_'+model,TopScales_e),
-   Channel("singlemuonwModel",  _wspace,out_ws,cid+'_'+model,TopScales_wm),
-   Channel("singleelectronwModel",  _wspace,out_ws,cid+'_'+model,TopScales_we),
+   Channel("singlemuonw0tagModel",  _wspace,out_ws,cid+'_'+model,TopScales0tag),
+   Channel("singleelectronw0tagModel",  _wspace,out_ws,cid+'_'+model,TopScales0tag_e),
+   Channel("singlemuonw1tagModel",  _wspace,out_ws,cid+'_'+model,TopScales1tag),
+   Channel("singleelectronw1tagModel",  _wspace,out_ws,cid+'_'+model,TopScales1tag_e),
+   Channel("singlemuonw2tagModel",  _wspace,out_ws,cid+'_'+model,TopScales2tag),
+   Channel("singleelectronw2tagModel",  _wspace,out_ws,cid+'_'+model,TopScales2tag_e),
+   Channel("singlemuonw2tagTo1tagModel",  _wspace,out_ws,cid+'_'+model,TopScales2tagTo1tag),
+   Channel("singleelectronw2tagTo1tagModel",  _wspace,out_ws,cid+'_'+model,TopScales2tagTo1tag_e)
   ]
 
 
@@ -69,7 +92,7 @@ def cmodel(cid,nam,_f,_fOut, out_ws, diag):
   # these must be created and writted to the same dirctory as the nominal (fDir)
  
   def addStatErrs(hx,cr,crname1,crname2):
-    for b in range(1,targetmc.GetNbinsX()+1):
+    for b in range(1,targetmc0tag.GetNbinsX()+1):
       err = hx.GetBinError(b)
       if not hx.GetBinContent(b)>0:
         continue
@@ -87,10 +110,14 @@ def cmodel(cid,nam,_f,_fOut, out_ws, diag):
       _fOut.WriteTObject(byb_d)
       cr.add_nuisance_shape('%s_stat_error_%s_bin%d'%(cid,crname2,b-1),_fOut)
 
-  addStatErrs(TopScales,CRs[0],'topme','muonelectron')
-  addStatErrs(TopScales_e,CRs[1],'topem','electronmuon')
-  addStatErrs(TopScales_wm,CRs[2],'wm','singlemuonw')
-  addStatErrs(TopScales_we,CRs[3],'we','singleelectronw')
+  addStatErrs(TopScales0tag,CRs[0],'topwm0tag','singlemuonw0tagModel')
+  addStatErrs(TopScales0tag_e,CRs[1],'topwe0tag','singleelectronw0tagModel')
+  addStatErrs(TopScales1tag,CRs[2],'topwm1tag','singlemuonw1tagModel')
+  addStatErrs(TopScales1tag_e,CRs[3],'topwe1tag','singleelectronw1tagModel')
+  addStatErrs(TopScales2tag,CRs[4],'topwm2tag','singlemuonw2tagModel')
+  addStatErrs(TopScales2tag_e,CRs[5],'topwe2tag','singleelectronw2tagModel')
+  addStatErrs(TopScales2tagTo1tag,CRs[6],'topwm2tagTo1tag','singlemuonw2tagTo1tagModel')
+  addStatErrs(TopScales2tagTo1tag_e,CRs[7],'topwe2tagTo1tag','singleelectronw2tagTo1tagModel')
 
   #######################################################################################################
 
